67fb47f7e4edeabedb6ef5312a5f25596805797a
Anant Singh <YOUR_GITHUB_EMAIL>
2026-04-01 01:05:35 +0530

    Add: application entry point with main method and welcome message

---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+
+public class MainApp {
+
+    /**
+     * Main method - starting point of the application
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay Application");
+        System.out.println("Version: 1.0");
+    }
+}
